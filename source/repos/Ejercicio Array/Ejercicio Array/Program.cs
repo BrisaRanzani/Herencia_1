72242315c4225c307eb3dbadf18364eea55af185
BrisaRanzani <brichuranzani@gmail.com>
2026-03-17 00:31:32 -0300

    este es otro cambio de prueba

--- source/repos/Ejercicio Array/Ejercicio Array/Program.cs
+++ source/repos/Ejercicio Array/Ejercicio Array/Program.cs
@@ -12,6 +12,7 @@ namespace Ejercicio_Array
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            //tercer commit de prueba
         }
     }
 }
